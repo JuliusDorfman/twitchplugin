2eb8221658bb0bb44d6154311c7b1882593cabda
JuliusDorfman <juliusgcd@gmail.com>
2022-10-16 12:31:52 -0700

    static paths for production

--- server/server.js
+++ server/server.js
@@ -11,6 +11,11 @@ const cors = require('cors');
 
 // connectDB();
 
+
+
+
+
+
 // ROUTES
 const app = express();
 
@@ -19,6 +24,20 @@ app.listen(port, () => console.log(chalk.blueBright(`Server started on port ${po
 app.use(express.urlencoded({extended: false}));
 app.use(cors());
 app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+  
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+
+
+
 
 // Handle Routes 
 app.use('/', require('./gamesRoutes'));
