6564f4d52a49df1cebaebb5f5d8857f7c9593313
JuliusDorfman <juliusgcd@gmail.com>
2022-10-16 11:15:42 -0700

    h3

--- src/Streams/Streams.jsx
+++ src/Streams/Streams.jsx
@@ -9,7 +9,8 @@ import Appbackground from '../Components/Appbackground';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button'
 const api = axios.create({
-    baseURL:  process.env.PORT || `http://localhost:7000/`
+    baseURL:  process.env.PORT 
+    // baseURL:  process.env.PORT || `http://localhost:7000/`
 });
 export default class Streams extends React.Component {
     constructor(props, ref) {
