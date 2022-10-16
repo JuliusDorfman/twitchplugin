c041bb0481c1882510f0e091f0e6e9996b8c05c4
JuliusDorfman <juliusgcd@gmail.com>
2022-10-16 13:07:42 -0700

    h5

--- server/server.js
+++ server/server.js
@@ -25,16 +25,16 @@ app.use(express.urlencoded({extended: false}));
 app.use(cors());
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'production') {
-    // Exprees will serve up production assets
-    app.use(express.static('client/build'));
+// if (process.env.NODE_ENV === 'production') {
+//     // Exprees will serve up production assets
+//     app.use(express.static('client/build'));
   
-    // Express serve up index.html file if it doesn't recognize route
-    const path = require('path');
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-  }
+//     // Express serve up index.html file if it doesn't recognize route
+//     const path = require('path');
+//     app.get('*', (req, res) => {
+//       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//     });
+//   }
 
 
 
