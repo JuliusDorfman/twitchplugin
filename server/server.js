292216ab1a1a22f86bbaf3266b31f2d7f4389196
JuliusDorfman <juliusgcd@gmail.com>
2022-10-16 13:32:43 -0700

    directory chages

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('/build'));
 	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+		res.sendFile(path.resolve(__dirname, '../', 'index.html'));
 	});
 }
 
