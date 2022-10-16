6bbe8f475fae986a79ee87505e0bbc265cffc386
JuliusDorfman <juliusgcd@gmail.com>
2022-10-16 13:13:56 -0700

    static paths

--- server/server.js
+++ server/server.js
@@ -25,16 +25,12 @@ app.use(express.urlencoded({extended: false}));
 app.use(cors());
 app.use(express.json());
 
-// if (process.env.NODE_ENV === 'production') {
-//     // Exprees will serve up production assets
-//     app.use(express.static('client/build'));
-  
-//     // Express serve up index.html file if it doesn't recognize route
-//     const path = require('path');
-//     app.get('*', (req, res) => {
-//       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//     });
-//   }
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('/build'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+	});
+}
 
 
 
