6aeff328db2d87b54f6183b4c5fb7b914ed50d8a
JuliusDorfman <juliusgcd@gmail.com>
2022-10-17 15:33:31 -0700

    change in static directory

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use('/api', require(path.resolve(__dirname, 'server', 'gamesRoutes'), next =
 // });
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('/build'));
+	app.use(express.static(__dirname));
 	app.get('*', (req, res) => {
 		res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 	});
