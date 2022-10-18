6916eea235427a02a0bae7d5740fd7496ba1a22f
JuliusDorfman <juliusgcd@gmail.com>
2022-10-17 19:48:03 -0700

    one more line to uncomment

--- src/Streams/Streams.jsx
+++ src/Streams/Streams.jsx
@@ -14,7 +14,7 @@ import Button from 'react-bootstrap/Button'
 
 let api = axios.create({
     baseURL: process.env.REACT_APP_API_URL,
-    baseURL: baseURL,
+    // baseURL: baseURL,
     mode: "cors",
 })
 
