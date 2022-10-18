2379eeaa24a12b540413127b380a0f59010fc739
JuliusDorfman <juliusgcd@gmail.com>
2022-10-17 17:27:40 -0700

    path.join in express static

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use('/api', require(path.resolve(__dirname, 'server', 'gamesRoutes.js'), nex
 // });
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(__dirname, 'build'));
+	app.use(express.static(path.join(__dirname, 'build')));
 	// app.get('*', (req, res) => {
 	app.get('/*', (req, res) => {
 		res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
